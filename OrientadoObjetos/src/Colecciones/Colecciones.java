a0de71e330f80226b6a0ba5f447c10e2771bae6d
Rigoberto Sanchez Jimenez <20243ds185@utez.edu.mx>
2026-03-31 13:56:12 -0600

    Practica poo terminada

--- OrientadoObjetos/src/Colecciones/Colecciones.java
+++ OrientadoObjetos/src/Colecciones/Colecciones.java
@@ -15,17 +15,40 @@ public class Colecciones {
         //HashSet no tiene un orden
         Set<String> conjunto1  = new HashSet<String>();
         conjunto1.add("Hola");
+        conjunto1.add("adios");
+
+        for (String s : conjunto1) {
+            System.out.println(s);
+        }
 
         //List si admite elementos duplicados y tiene una posicion
         //ArrayList
         List<Libro> conjunto2 = new ArrayList<Libro>();
+        Libro libro1 = new Libro();
+        Libro libro2 = new Libro();
+        conjunto2.add(libro1);
+        conjunto2.add(1, libro2);
+        conjunto2.isEmpty();
+
+        for(Libro l: conjunto2){
+            System.out.println(l.toString());
+        }
         
+        System.out.println();
         
         //Map
         //HashMap
         Map<String, Libro> conjunto3 = new HashMap<String, Libro>();
-        
+        conjunto3.put("Elemento1", libro1);
+        conjunto3.put("Elemto2", libro2);
+        conjunto3.get("Elemto1");
+        for(String clave:conjunto3.keySet()){
+            Libro l = (Libro) conjunto3.get(clave);
+            System.out.println(l.toString());
+        }
 
+
+        
         
 
     }
